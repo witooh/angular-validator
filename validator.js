50e8fc966d44fc07f463a03b34d0342db41578af
witooh <witoo@outlook.com>
2013-05-20 13:18:19 +0700

    initial

---
+++ validator.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+App.Services.
+    factory('validator', ['$rootScope', function ($rootScope) {
+        var validateService = {};
+
+        validateService.make = function (selector, config) {
+            var defaultConfig = {
+                onkeyup: false,
+                errorElement: 'span',
+                errorClass: 'help-inline',
+                highlight: function (element) {
+                    $(element).closest('.control-group').addClass('error');
+                },
+                unhighlight: function (element) {
+                    $(element).closest('.control-group').removeClass('error');
+                },
+                submitHandler: function (form) { }
+            };
+            config = $.extend(defaultConfig, config);
+            $(selector).validate(config);
+        };
+        return validateService;
+    }]);
